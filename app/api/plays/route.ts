009ab0f15d8351ab21e3ae4f7581f1da904aab55
Chris Barthol <chris.barthol@gmail.com>
2025-01-09 18:21:50 -0500

    failure

--- app/api/plays/route.ts
+++ app/api/plays/route.ts
@@ -1,6 +1,6 @@
 import { bgg } from "bgg-sdk";
 
-export async function GET(username: string) {
-    const results = await bgg.plays.username({ username: 'chrisbarthol'});
+export async function GET() {
+    const results = await bgg.plays.username({ username: 'username'});
     return Response.json(results);
 };
