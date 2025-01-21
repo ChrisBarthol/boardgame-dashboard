ccb81b0a834334864df7b8bf3b86a57dfbc800cd
Chris Barthol <chris.barthol@gmail.com>
2025-01-21 09:01:56 -0500

    parse the plays response

--- app/api/plays/route.ts
+++ app/api/plays/route.ts
@@ -2,5 +2,22 @@ import { bgg } from "bgg-sdk";
 
 export async function GET() {
     const results = await bgg.plays.username({ username: 'username'});
-    return Response.json(results);
+
+    try {
+        if (!results) {
+            throw new Error("No results returned from bgg.plays.username");
+        }
+        return Response.json(results.plays.map((play: any) => ({
+            id: play.id,
+            date: play.date,
+            quantity: play.quantity,
+            gameName: play.item.name,
+            gameId: play.item.objectid,
+            subtypes: play.item.subtypes
+        })));
+    } catch (error) {
+        console.error("Invalid JSON string", error);
+        return [];
+    }
 };
+
